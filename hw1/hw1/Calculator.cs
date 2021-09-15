c60d09ff0559b149d1e864ca056fc0cff454d172
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2021-09-15 18:15:53 +0300

    [add]hw1

---
+++ hw1/hw1/Calculator.cs
@@ -0,0 +1,26 @@
+﻿namespace Hw1
+{
+    internal static class Calculator
+    {
+        internal static int Calculate(Program.Operation operation, int val1, int val2)
+        {
+            var result = 0;
+            switch (operation)
+            {
+                case Program.Operation.Add:
+                    result = val1 + val2;
+                    break;
+                case Program.Operation.Subtract:
+                    result = val1 - val2;
+                    break;
+                case Program.Operation.Multiply:
+                    result = val1 * val2;
+                    break;
+                case Program.Operation.Divide:
+                    result = val1 / val2;
+                    break;
+            }
+            return result;
+        }
+    }
+}

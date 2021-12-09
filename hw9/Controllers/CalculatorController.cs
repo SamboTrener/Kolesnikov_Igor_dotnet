560577b0a3c955d6c11f8fee78a881b3c41476d0
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2021-12-09 12:30:43 +0300

    it works

---
+++ hw9/Controllers/CalculatorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using hw9.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hw9.Controllers
+{
+    
+    public class CalculatorController : Controller
+    {
+        public IActionResult Calculate([FromServices] ICalculator calculator, string stringToParse)
+        {
+            return Content(calculator.Calculate(stringToParse).ToString(CultureInfo.InvariantCulture));;
+        }
+    }
+}

7e0b27a33459d35aa0e02e19568760dad442d250
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2021-12-14 20:50:19 +0300

    added hw10

---
+++ hw10/hw10/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace hw10.Models
+{
+    public class ErrorViewModel
+    {
+        
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
